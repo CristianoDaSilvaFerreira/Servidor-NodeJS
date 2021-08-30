45903d9f987d482fef9ba7cb2c56b0997934a0b5
CRISTIANO DA SILVA FERREIRA <68359459+CristianoDaSilvaFerreira@users.noreply.github.com>
2021-08-30 18:14:54 -0300

    :up:  listando um usuário em especifico

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
 
   route.get((req, res) => {
 
-    db.find({}).sort({name: 1}).exec((err, user) => {
+    db.find({ }).sort({ name: 1 }).exec((err, user) => {
 
       if (err) {
         app.utils.error.send(err, req, res);
@@ -36,7 +36,23 @@ module.exports = (app) => {
 
         res.status(200).json(user);
       }
-    })
+    });
+  });
+
+  let routeId = app.route('/users/:id');
+
+  routeId.get((req, res) => {
+
+    db.findOne({ _id: req.params.id }).exec((err, user) => {
+
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+
+        res.status(200).json(user);
+      }
+    });
+
   });
 
 }
